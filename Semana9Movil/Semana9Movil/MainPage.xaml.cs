cd240743f8b3d6f29d8a01f6045226b3fd128d36
pmomtoyam <pmontoyam245@ulacit.ed.cr>
2023-07-10 22:11:42 -0600

    Comit inicial

---
+++ Semana9Movil/Semana9Movil/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Semana9Movil.Model;
+
+namespace Semana9Movil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Semana9Movil/Semana9Movil/Model/CAT_USUARIO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana9Movil.Model
+{
+    public class CAT_USUARIO
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+        public string Activa { get; set; }
+    }
+
+
+    public class links
+    {
+        public string rel { get; set; }
+        public string href { get; set; }
+
+        public override string ToString()
+        {
+            return href;
+        }
+    }
+
+
+    public class ArrayProblem
+    {
+        public CAT_USUARIO[] items { get; set; }
+        public bool hasMore { get; set; }
+        public bool limit { get; set; }
+        public bool offset { get; set; }
+        public bool count { get; set; }
+        public links[] links { get; set; }
+    }
+}
